dfc4e5fc2bc2c5c4e1326b26b07ada8e9cfc5ed3
sugaret <cigaret.cigaret@gmail.com>
2022-07-23 02:05:32 +0000

    installとanalizeを追加

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -ex
+
+sudo apt-get install -y dstat unzip graphviz
+
+# alp
+cd /var/tmp/ && wget https://github.com/tkuchiki/alp/releases/download/v0.4.0/alp_linux_amd64.zip
+unzip alp_linux_amd64.zip
+sudo mv ./alp /usr/local/bin
+
+# pt-query-digest
+cd /var/tmp && wget https://www.percona.com/downloads/percona-toolkit/3.0.13/binary/tarball/percona-toolkit-3.0.13_x86_64.tar.gz
+tar -zxvf percona-toolkit-3.0.13_x86_64.tar.gz
+sudo mv ./percona-toolkit-3.0.13/bin/pt-query-digest /usr/local/bin
+
