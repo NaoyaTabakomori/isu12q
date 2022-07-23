a91ab8d1308c9f7716b7dba86ddded4ba2f9b81a
naoya.tabakomori <cigaret.cigaret@gmail.com>
2022-07-23 14:02:33 +0900

    alpログをファイルに出力

--- analize.sh
+++ analize.sh
@@ -8,7 +8,7 @@ mkdir -p ${LOG_DIR}
 if [ -f ${LOG_DIR}/alp.log ]; then
     sudo mv ${LOG_DIR}/alp.log ${LOG_DIR}/alp.log.$(date "+%Y%m%d_%H%M%S")
 fi
-sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r --aggregates='/api/player/competition/[a-z0-9]+/ranking$,/api/player/player/[a-z0-9]+$,/api/player/competition/[a-z0-9]+/score$,/api/organizer/competition/[a-z0-9]+/score$,/api/organizer/competition/[a-z0-9]+/finish$,/api/organizer/competition/[a-z0-9]+/score$,/api/organizer/competition/[a-z0-9]+/finish$,/api/organizer/player/[a-z0-9]+/disqualified$'
+sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r --aggregates='/api/player/competition/[a-z0-9]+/ranking$,/api/player/player/[a-z0-9]+$,/api/player/competition/[a-z0-9]+/score$,/api/organizer/competition/[a-z0-9]+/score$,/api/organizer/competition/[a-z0-9]+/finish$,/api/organizer/competition/[a-z0-9]+/score$,/api/organizer/competition/[a-z0-9]+/finish$,/api/organizer/player/[a-z0-9]+/disqualified$'  > ${LOG_DIR}/alp.log
 
 # slow query
 if [ -f ${LOG_DIR}/mysql-slow.log ]; then
