093680e1d4b43eca4a50e4c7ee1a41616784c1c2
lazydg <1732998+lazydg@users.noreply.github.com>
2022-07-23 12:49:21 +0900

    fix index

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -32,4 +32,4 @@ CREATE TABLE player_score (
   created_at BIGINT NOT NULL,
   updated_at BIGINT NOT NULL
 );
-CREATE INDEX i1 ON player_score(tenant_id,competition_id,player_id);
+CREATE INDEX i1 ON player_score(tenant_id,competition_id,player_id,row_num);
